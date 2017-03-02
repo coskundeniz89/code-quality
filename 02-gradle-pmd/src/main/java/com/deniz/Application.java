f185596ae6e7d8c0e4f5d35f5e16b307a2f838f8
Coşkun Deniz <coskundeniz1989@gmail.com>
2017-03-02 22:11:34 +0300

    Added pmd example

---
+++ 02-gradle-pmd/src/main/java/com/deniz/Application.java
@@ -0,0 +1,15 @@
+package com.deniz;
+
+/**
+ * Created by Coşkun on 6.2.2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        if (1 == 1) {
+            if (1 == 1) {
+                System.out.printf("Pmd kızar beyler...");
+            }
+        }
+        System.out.println("Türkiye");
+    }
+}
